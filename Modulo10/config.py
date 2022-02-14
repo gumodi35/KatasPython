88b5046ca59afada92b2182c00f8f4ea3f412ea5
gumodi35 <gumodi10@gmail.com>
2022-02-14 13:39:56 -0600

    Subiendo Katas Python

---
+++ Modulo10/config.py
@@ -0,0 +1,12 @@
+def main():
+    try: 
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but is a directory, couldn't read it")
+    except (BlockingIOError,TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
+        
+if __name__ == '__main__':
+    main()
